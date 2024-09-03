50f74755cdf0b6c70ff5c69fbb0597c338099bef
Muhammad Dzakwan Attaqiy <98676984+mhddzakwan@users.noreply.github.com>
2024-09-03 20:07:32 +0700

    Update send.php

--- api/send.php
+++ api/send.php
@@ -5,12 +5,6 @@ use Google\Auth\HttpHandler\HttpHandlerFactory;
 
 require 'vendor\autoload.php';
 
-$jsonData = file_get_contents("php://input");
-
-$data = json_decode($jsonData, true);
-
-$message = $data["message"];
-
 $credential = new ServiceAccountCredentials(
     "https://www.googleapis.com/auth/firebase.messaging",
     json_decode(file_get_contents("pvKey.json"), true)
@@ -18,25 +12,39 @@ $credential = new ServiceAccountCredentials(
 
 $token = $credential->fetchAuthToken(HttpHandlerFactory::build());
 
-$ch = curl_init("https://fcm.googleapis.com/v1/projects/flodec-p24s/messages:send");
+$ch = curl_init("https://fcm.googleapis.com/v1/projects/belajar-real-time/messages:send");
 
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
     'Content-Type: application/json',
     'Authorization: Bearer '.$token['access_token']
 ]);
 
+$deviceTokens = [
+    "fQxZLpm3QuyKt6s6XEgSFr:APA91bFokP_lAE25IUuK8OTuXe0PSzWbNNdAyFAVPrtGQ6WEPWGKj3cU5SXYt5tBsHnxDuo8fJbhTqExuXIOgbw-PizHHDZHBHQnvcs_qjjaX6xvLaLEtg9yI6iOjhzIGtGL3oFLt3PT",
+    //"d3HyBNn3Tf6F9Z6v2jJYNZ:APA91bHN6qm64nxBKtg5xp_ZPKrol649kImisNJ2_idCG0BKXtPPstw4mWnTX5S__0wgrwU8wP6IqI9aOjDqHKwe-iYLHHgk12BebV_afI6hN_VJUY86Apef2KkW0jJ0mdXV0bTEYuKB"
+];
+
+// curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+foreach ($deviceTokens as $token) {
     $payload = json_encode([
         "message" => [
-            "topic" => "notif",
+            "token" => $token,
             "notification" => [
-                "body" => $message,
+                "body" => "Hujan deras terjadi di Jl. Gaperta Ujung, 10 menit lagi akan banjir nih",
                 "title" => "Flodecs"
+            ],
+            "data" => [
+                "Status" => "Banjir",
+                "Ketinggian" => "10 cm",
+                "Intensitas" => "7 mm/jam",
+                "Debit" => "40 mm^3/menit"
             ],
             "android" => [
                 "notification" => [
                   "icon" => "baseline_info_24"
                 ]
             ],
+            
         ]
     ]);
 
@@ -47,5 +55,6 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, [
     $response = curl_exec($ch);
     
     echo $response;
+}
 
 curl_close($ch);
